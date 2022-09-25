b942c4ed6e700ea5f2caaa13fb319e789df38f8e
tattsun <dev.tattsun@gmail.com>
2022-09-25 13:49:26 +0900

    wip

--- senseki.js
+++ senseki.js
@@ -1,5 +1,11 @@
 var playerName = '';
 
+function getElementCountByXPath(xpath) {
+    const res = document.evaluate(`count(${xpath})`, document, null, XPathResult.ANY_TYPE, null);
+    console.log(xpath, res);
+    return res.numberValue;
+}
+
 function setGetNameHook() {
     const query = "#main > div > div:nth-child(3) > div:nth-child(6) > input[type=text]";
     const elem = document.querySelector(query);
@@ -13,23 +19,11 @@ function setGetNameHook() {
 }
 
 function getIsGameDone() {
-    const query = '#main > div > div:nth-child(3) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > span';
-    const elem = document.querySelector(query);
-    return elem != null;
+    return getElementCountByXPath(`//span[text()='勝利']`) > 0;
 }
 
-function clickShowRatingButton() {
-    const query = "#main > div > div:nth-child(3) > div:nth-child(2) > div:nth-child(8)";
-    const elem = document.querySelector(query);
-    if (elem) {
-        elem.click();
-    }
-}
-
-function getWinnerName() {
-    const query = "#main > div > div:nth-child(3) > div:nth-child(2) > div:nth-child(3) > div:nth-child(2) > div > span";
-    const elem = document.querySelector(query);
-    return elem.innerHTML;
+function getIsWin() {
+    return getElementCountByXPath(`//span[text()='${playerName}']`) > 3;
 }
 
 setGetNameHook();
@@ -37,7 +31,7 @@ setGetNameHook();
 setInterval(() => {
     console.log(playerName);
     if (getIsGameDone()) {
-        console.log(`戦闘終了: 勝者 ${getWinnerName()}`);
+        console.log(`戦闘終了: ${getIsWin()}`);
     } else {
         console.log("戦闘未終了");
     }
