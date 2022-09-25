8be5f2a0b9b72afb109cd15ebdcac3b2748d2e5b
tattsun <dev.tattsun@gmail.com>
2022-09-25 14:52:10 +0900

    fix

--- senseki.js
+++ senseki.js
@@ -1,39 +1,100 @@
-var playerName = '';
-
-function getElementCountByXPath(xpath) {
-    const res = document.evaluate(`count(${xpath})`, document, null, XPathResult.ANY_TYPE, null);
-    console.log(xpath, res);
-    return res.numberValue;
-}
-
-function setGetNameHook() {
-    const query = "#main > div > div:nth-child(3) > div:nth-child(6) > input[type=text]";
-    const elem = document.querySelector(query);
-    if (!elem) {
-        return;
+class BrowserAdapter {
+    getElementsByXPath(xpath) {
+        const res = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+        let curr = res.iterateNext();
+        const elems = [];
+        while (curr) {
+            elems.push(curr);
+            curr = res.iterateNext();
+        }
+        return elems;
     }
-    playerName = elem.value;
-    elem.addEventListener('change', (event) => {
-        playerName = event.target.value;
-    });
 }
 
-function getIsGameDone() {
-    return getElementCountByXPath(`//span[text()='勝利']`) > 0;
-}
-
-function getIsWin() {
-    return getElementCountByXPath(`//span[text()='${playerName}']`) > 3;
-}
-
-setGetNameHook();
-
-setInterval(() => {
-    console.log(playerName);
-    if (getIsGameDone()) {
-        console.log(`戦闘終了: ${getIsWin()}`);
-    } else {
-        console.log("戦闘未終了");
+class GameWatcher {
+    constructor(browserAdapter) {
+        this._browserAdapter = browserAdapter;
+        this._playerName = '';
     }
+
+    setHook() {
+        const xpath = '//*[@id="main"]/div/div[3]/div[6]/input';
+        const elems = this._browserAdapter.getElementsByXPath(xpath);
+        if (elems.length !== 1) {
+            throw new Error(`invalid elem count: ${elems}`);
+        }
+        this._playerName = elems[0].value;
+        elems[0].addEventListener('change', (ev) => {
+            this._playerName = ev.target.value;
+        });
+    }
+
+    getPlayerName() {
+        return this._playerName;
+    }
+
+    /**
+     * 現状のゲームモードを特定する
+     * @returns {string} 'HIDDEN' (隠れ乱闘), 'UNKNOWN' (未対応)
+     */
+    getGameMode() {
+        const xpath = '//*[@id="main"]/div/div[1]/span';
+        const elems = this._browserAdapter.getElementsByXPath(xpath);
+
+        if (elems.length !== 1) {
+            return 'UNKNOWN';
+        }
+
+        switch (elems[0].innerHTML) {
+            case '隠れ乱闘':
+                return 'HIDDEN';
+            default:
+                return 'UNKNOWN';
+        }
+    }
+}
+
+if (typeof window === 'undefined') {
+    module.exports = {
+        GameWatcher,
+        BrowserAdapter,
+    };
+} else {
+    const watcher = new GameWatcher(new BrowserAdapter());
+    watcher.setHook();
+
+    setInterval(() => {
+        console.log(`Player name: ${watcher.getPlayerName()}`);
+    }, 1000);
+}
+
+// class HiddenMelee {
+//     constructor() {
+//     }
+// }
+
+// function getElementCountByXPath(xpath) {
+//     const res = document.evaluate(`count(${xpath})`, document, null, XPathResult.ANY_TYPE, null);
+//     console.log(xpath, res);
+//     return res.numberValue;
+// }
+
+// function getIsGameDone() {
+//     return getElementCountByXPath(`//span[text()='勝利']`) > 0;
+// }
+
+// function getIsWin() {
+//     return getElementCountByXPath(`//span[text()='${playerName}']`) > 3;
+// }
+
+
+// setInterval(() => {
+//     console.log(playerName);
+//     if (getIsGameDone()) {
+//         console.log(`戦闘終了: ${getIsWin()}`);
+//     } else {
+//         console.log("戦闘未終了");
+//     }
     
-}, 1000);
+// }, 1000);
+
---
+++ senseki.spec.js
@@ -0,0 +1,119 @@
+const { GameWatcher, BrowserAdapter } = require('./senseki');
+
+class BrowserAdapterMock {
+    constructor() {
+        this._xpathMocks = {};
+    }
+
+    registerMockByXPath(xpath, elem) {
+        let elems = this._xpathMocks[xpath];
+        if (!elems) {
+            elems = [];
+            this._xpathMocks[xpath] = elems;
+        }
+        elems.push(elem);
+    }
+
+    getElementsByXPath(xpath) {
+        return this._xpathMocks[xpath] || [];
+    }
+}
+
+describe('GameWatcher', () => {
+    const nameInputXPath = '//*[@id="main"]/div/div[3]/div[6]/input';
+
+    let adapter;
+    let watcher;
+
+    beforeEach(() => {
+        adapter = new BrowserAdapterMock();
+        watcher = new GameWatcher(adapter);
+    });
+
+    function newMockElementHasValue(value) {
+        const eventListeners = [];
+
+        return {
+            value,
+            addEventListener: (_, listener) => eventListeners.push(listener),
+            dispatch: (newValue) => eventListeners.forEach((listener) => listener({
+                target: {
+                    value: newValue,
+                },
+            })),
+        }
+    }
+
+    function newMockElementHasInnerHTML(innerHTML) {
+        return {
+            innerHTML,
+        }
+    }
+
+    describe('getPlayerName()', () => {
+        it('returns empty string when not initialized', () => {
+            expect(watcher.getPlayerName()).toEqual('');
+        });
+
+        it('can get the default player name', () => {
+            adapter.registerMockByXPath(nameInputXPath, newMockElementHasValue('some player'));
+            watcher.setHook();
+            
+            expect(watcher.getPlayerName()).toEqual('some player');
+        });
+
+        it('can get the another default player name', () => {
+            adapter.registerMockByXPath(nameInputXPath, newMockElementHasValue('another player'));
+            watcher.setHook();
+
+            expect(watcher.getPlayerName()).toEqual('another player');
+        });
+
+        it('can detect value change', () => {
+            const elem = newMockElementHasValue('initial name') ;
+            adapter.registerMockByXPath(nameInputXPath, elem);
+            watcher.setHook();
+
+            expect(watcher.getPlayerName()).toEqual('initial name');
+
+            elem.dispatch('new name');
+            expect(watcher.getPlayerName()).toEqual('new name');
+        });
+
+        it('throws error when elem not found', () => {
+            expect(() => {
+                watcher.setHook();
+            }).toThrow();
+        });
+
+        it('throws error when too many elems', () => {
+            adapter.registerMockByXPath(nameInputXPath, newMockElementHasValue('hoge'));
+            adapter.registerMockByXPath(nameInputXPath, newMockElementHasValue('fuga'));
+            expect(() => {
+                watcher.setHook();
+            }).toThrow();
+        });
+    });
+
+    describe('getGameMode', () => {
+        const gameModeXPath = '//*[@id="main"]/div/div[1]/span';
+
+        beforeEach(() => {
+            adapter.registerMockByXPath(nameInputXPath, newMockElementHasValue('hoge'));
+        });
+
+        it('returns UNKNOWN on a unknown game', () => {
+            expect(watcher.getGameMode()).toEqual('UNKNOWN');
+        });
+
+        it('returns UNKNOWN on a 真剣タイマン', () => {
+            adapter.registerMockByXPath(gameModeXPath, newMockElementHasInnerHTML('真剣タイマン'));
+            expect(watcher.getGameMode()).toEqual('UNKNOWN');
+        });
+
+        it('returns HIDDEN on a hidden melee', () => {
+            adapter.registerMockByXPath(gameModeXPath, newMockElementHasInnerHTML('隠れ乱闘'));
+            expect(watcher.getGameMode()).toEqual('HIDDEN');
+        });
+    });
+});
